8a8ec4f29e4e2d397e7f887cdd0cda8e6653dba3
asnganje <abd.nganje@gmail.com>
2025-03-27 18:17:11 +0300

    add classes separation logic

---
+++ teacher.rb
@@ -0,0 +1,13 @@
+require_relative 'person'
+class Teacher < Person
+  attr_accessor :specialization
+
+  def initialize(specialization, age, name = 'Unknown')
+    super(age, name)
+    @specialization = specialization
+  end
+
+  def can_use_services?
+    true
+  end
+end
