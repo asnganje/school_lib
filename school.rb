b9e225824329117d33859d977fa906fec70d4c1b
asnganje <abd.nganje@gmail.com>
2025-03-27 17:09:10 +0300

    fix linter errors

--- school.rb
+++ school.rb
@@ -1,11 +1,12 @@
 class Person
   attr_accessor :name, :age
   attr_reader :id
-  def initialize(name="Unknown", age, parent_permission=true)
-    @id=rand(1..100)
-    @name=name
-    @age=age
-    @parent_permission = parent_permission    
+
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    @id = rand(1..100)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
   end
 
   def can_use_services?
@@ -13,6 +14,7 @@ class Person
   end
 
   private
+
   def of_age?
     @age >= 18
   end
