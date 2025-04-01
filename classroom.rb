64880122bbf218be041467841608024526c4ac13
asnganje <abd.nganje@gmail.com>
2025-04-01 15:39:48 +0300

    Add relationship between classroom and student

--- classroom.rb
+++ classroom.rb
@@ -1,6 +1,14 @@
 class ClassRoom
-  attr_accessor :label
+  attr_accessor :label, :students
   def initialize(label)
     @label = label
+    @student = []
+  end
+
+  def add_student(student)
+    unless @students.include?(student)
+      @student << student
+      student.classroom = self unless student.classroom == self
+    end
   end
 end
--- student.rb
+++ student.rb
@@ -2,9 +2,16 @@ require_relative 'person'
 class Student < Person
   attr_accessor :classroom
 
-  def initialize(classroom, age, name = 'Unknown', parent_permission: true)
+  def initialize(classroom=nil, age, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission: parent_permission)
+    self.classroom = classroom if classroom
+  end
+
+  def classroom=(classroom)
+    return if @classroom == classroom
+    @classroom&.students&.delete(self)
     @classroom = classroom
+    classroom.add_student(self) unless classroom.nil? || classroom.students.include?(self)
   end
 
   def play_hooky
