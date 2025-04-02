3506615610234b50956e18d148fa3a1bc566d0e9
asnganje <abd.nganje@gmail.com>
2025-04-02 07:36:55 +0300

    Fix rental_tests linter errors

--- spec/unit/rental_tests.rb
+++ spec/unit/rental_tests.rb
@@ -3,21 +3,21 @@ require_relative '../../book'
 require_relative '../../person'
 
 describe Rental do
-  let!(:book) {Book.new( "First Book", "Abdul")}
-  let!(:person) {Person.new(2, 30)}
-  let!(:rental) {Rental.new("2025-04-03", book, person)}
-  describe "validates instance attributes" do
-    it "should validate instance of Rental" do
+  let!(:book) { Book.new('First Book', 'Abdul') }
+  let!(:person) { Person.new(2, 30) }
+  let!(:rental) { Rental.new('2025-04-03', book, person) }
+  describe 'validates instance attributes' do
+    it 'should validate instance of Rental' do
       expect(rental).to be_an_instance_of(Rental)
-    end 
+    end
   end
 
-  describe "validates associations" do
-    it "should associate a book with a rental" do
+  describe 'validates associations' do
+    it 'should associate a book with a rental' do
       expect(rental.book).to eq(book)
     end
 
-    it "should associate person with a rental" do
+    it 'should associate person with a rental' do
       expect(rental.person).to eq(person)
     end
   end
