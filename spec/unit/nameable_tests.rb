94e195148c8729e49beff7b733e6c4328708efdc
asnganje <abd.nganje@gmail.com>
2025-04-02 07:45:36 +0300

    Add nameable test

---
+++ spec/unit/nameable_tests.rb
@@ -0,0 +1,9 @@
+require_relative '../../nameable'
+
+describe Nameable do
+  describe "runs correct name method" do
+    it "it should raise an error" do
+      expect{Nameable.new.correct_name}.to raise_error(NotImplementedError)
+    end
+  end
+end
