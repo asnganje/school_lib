ccc9878cf09ab9f008dd254880059910d74d3f7c
asnganje <abd.nganje@gmail.com>
2025-04-02 06:54:26 +0300

    Add tests for person class and methods, include linter checks

--- spec/unit/book_tests.rb
+++ spec/unit/book_tests.rb
@@ -1,19 +1,19 @@
 require_relative '../../book'
 describe Book do
-  let!(:bk) {Book.new("First book", "Abdul")}
-  describe "Validates attributes" do
-    it "should ensure attributes are valid" do
+  let!(:bk) { Book.new('First book', 'Abdul') }
+  describe 'Validates attributes' do
+    it 'should ensure attributes are valid' do
       expect(bk).to be_an_instance_of(Book)
     end
   end
 
-  describe "adds rental to book" do
-    it "should add rental to @rentals array" do
-      rental = {person: "Abdul", date:"2024-05-12", book:"book"}
+  describe 'adds rental to book' do
+    it 'should add rental to @rentals array' do
+      rental = { person: 'Abdul', date: '2024-05-12', book: 'book' }
       bk.add_rental(rental)
 
       expect(bk.rentals).to include(rental)
-      expect(bk.rentals.size).to eq(1) 
+      expect(bk.rentals.size).to eq(1)
     end
   end
 end
